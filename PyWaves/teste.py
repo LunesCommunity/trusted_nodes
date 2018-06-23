df4023bed4ca6b3c73dd4b8fc635f25851f4a4d9
Daniel Checchia <daniel@checchia.net>
2018-06-23 12:44:05 -0300

    first commit

--- PyWaves/teste.py
+++
@@ -1,5 +0,0 @@
-import inicio as pw
-
-myAddress = pw.Address(privateKey='CtMQWJZqfc7PRzSWiMKaGmWFm4q2VN5fMcYyKDBPDx6S')
-otherAddress = pw.Address('3PNTcNiUzppQXDL9RZrK3BcftbujiFqrAfM')
-myAddress.sendWaves(otherAddress, 10000000)
