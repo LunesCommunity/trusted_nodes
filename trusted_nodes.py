48fcca2810c61a707131e88c393fe8093e39340f
Rui Andrada <shingonoide@gmail.com>
2018-07-04 14:50:21 -0300

    Cleanup unnecessary code

--- trusted_nodes.py
+++ trusted_nodes.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
-import PyWaves as pw
+
 import requests
 import math
 import json
 import re
-import urllib2
 import io
 
 node = 'http://api.lunes.in'
@@ -50,6 +49,7 @@ def get_address(url):
 for n in range(0, int(math.ceil(nblocks / 100.0))):
     for block in requests.get('%s/blocks/seq/%d/%d' % (node, last - nblocks + n * 100, min(last, last - nblocks + (n + 1) * 100 - 1))).json():
         generators.append((str(block['generator']), float(block['fee']) / 100000000))
+
 for generator in set([x[0] for x in generators]):
     fees = sum(g[1] for g in filter(lambda x: x[0] == generator, generators))
     count = sum(1 for g in filter(lambda x: x[0] == generator, generators))
@@ -57,6 +57,7 @@ for generator in set([x[0] for x in generators]):
     generator_balance = float(requests.get(node + '/consensus/generatingbalance/' + generator).json()['balance']) / 100000000
     unique_generators.append((generator, generator_balance, count, fees))
     total_balance += generator_balance
+
 for i, generator in enumerate(sorted(unique_generators, key=lambda x: -x[1])):
     char_list = ['\[', ',', '\]', '\"', "\'"]
     alias = json.dumps( requests.get (node + '/addresses/alias/by-address/' + generator[0]).json())
